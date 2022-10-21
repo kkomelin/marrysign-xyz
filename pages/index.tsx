3da35a8ac1fddf27b67be293465390969a201f62
Konstantin Komelin <konstantin.komelin@gmail.com>
2022-10-21 23:34:59 +0300

    Improved description..

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const title = "MarrySign";
-  const description = "We empower couples to register their marriage online";
+  const description = "We empower couples to register marriage online";
   return (
     <div className={styles.container}>
       <Head>
