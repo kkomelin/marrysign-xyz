593cba3c4591a207d7a4f910598e3027b0c37f48
Konstantin Komelin <konstantin.komelin@gmail.com>
2022-10-26 05:02:29 +0300

    Improved wallet list.

--- lib/auth/init.ts
+++ lib/auth/init.ts
@@ -1,51 +1,42 @@
 import { connectorsForWallets } from '@rainbow-me/rainbowkit'
 import {
   coinbaseWallet,
+  injectedWallet,
   metaMaskWallet,
   trustWallet,
   walletConnectWallet,
 } from '@rainbow-me/rainbowkit/wallets'
 import { chain, configureChains, createClient } from 'wagmi'
-import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
 import { APP_NAME } from '../config'
 import { isProd } from '../helpers'
 
-let networkds = [
-  chain.mainnet,
-  // chain.polygon,
-  // chain.optimism,
-  // chain.arbitrum
-]
+let networkds = [chain.mainnet]
 
 if (!isProd()) {
-  networkds = [
-    // chain.goerli, 
-    chain.hardhat, 
-    // chain.localhost
-  ]
+  networkds = [chain.goerli, chain.hardhat, chain.localhost]
 }
 
-const { chains, provider } = configureChains(networkds, [
-  alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-  publicProvider(),
-])
+const { chains, provider } = configureChains(networkds, [publicProvider()])
 
 const connectors = connectorsForWallets([
   {
     groupName: 'Popular',
     wallets: [
       metaMaskWallet({ chains }),
-      coinbaseWallet({ appName: APP_NAME, chains }),
-      walletConnectWallet({ chains }),
+      coinbaseWallet({
+        chains,
+        appName: APP_NAME,
+      }),
       trustWallet({ chains }),
+      walletConnectWallet({ chains }),
+      injectedWallet({
+        chains,
+        shimDisconnect: true,
+      }),
     ],
   },
 ])
-// const { connectors } = getDefaultWallets({
-//   appName: APP_NAME,
-//   chains,
-// })
 
 const wagmiClient = createClient({
   autoConnect: true,
