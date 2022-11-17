2ebbb2e97db05d1b897b93fb9979baa3514c7cbd
Konstantin Komelin <konstantin.komelin@gmail.com>
2022-11-17 11:53:23 +0300

    Optimizing gas limit for the termination function.

--- lib/services/agreement/index.ts
+++ lib/services/agreement/index.ts
@@ -134,7 +134,7 @@ export const terminateAgreement = async (
 
   const receipt = await contract.terminateAgreement(id, {
     value: terminationCost,
-    gasLimit: 800000 // 3000000, // Have to explicitly set it here to prevent error.
+    gasLimit: 500000 // 3000000, // Have to explicitly set it here to prevent error.
   })
 
   const result = await receipt.wait(1)
