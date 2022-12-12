14693f10599a56375840901f5c43008c77e4caab
Konstantin Komelin <konstantin.komelin@gmail.com>
2022-12-12 13:00:37 +0300

    Installed HotJar code.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,22 +8,41 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {isProduction && gaId && (
+        {isProduction && (
           <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-            />
+            {gaId && (
+              <>
+                <script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+                />
+                <script
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${gaId}', {
+                        page_path: window.location.pathname,
+                      });
+                    `,
+                  }}
+                />
+              </>
+            )}
+
             <script
               dangerouslySetInnerHTML={{
                 __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaId}', {
-                  page_path: window.location.pathname,
-                });
-              `,
+                  (function(h,o,t,j,a,r){
+                      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                      h._hjSettings={hjid:3284638,hjsv:6};
+                      a=o.getElementsByTagName('head')[0];
+                      r=o.createElement('script');r.async=1;
+                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                      a.appendChild(r);
+                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                  `,
               }}
             />
           </>
