e5277dd68c4cd6abd8a2a26db96acb85a91c1bc2
Konstantin Komelin <konstantin.komelin@gmail.com>
2022-12-24 23:07:12 +0300

    Changed counter bar background.

--- pages/index.tsx
+++ pages/index.tsx
@@ -170,7 +170,7 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <div className="w-full px-6 py-10 text-2xl font-semibold text-center text-white uppercase bg-blue-400">
+      <div className="w-full px-6 py-10 text-2xl font-semibold text-center text-white uppercase bg-violet-400">
         {agreementCount > 0 ? (
           <div>
             {agreementCount} agreement{agreementCount > 1 ? 's' : ''} created
