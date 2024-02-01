6fbecc6da9debf857789efbd79cb60a728358474
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-02-01 14:51:12 +0700

    Removed the important setting which was added by mistake.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@
 const { fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  important: true,
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
